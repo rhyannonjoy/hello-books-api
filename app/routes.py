193ffcb56c5a64f42341e8e82a2951813760e1fb
Rhyannon <rhyannonjoy@gmail.com>
2021-10-14 22:26:24 -0600

    Two initial endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,16 @@
 from flask import Blueprint
 
+hello_world_bp = Blueprint("hello_world", __name__)
+
+@hello_world_bp.route("/hello-world", methods=["GET"])
+def say_hello_world():
+    my_beautiful_response_body = "Hello, World!"
+    return my_beautiful_response_body
+
+@hello_world_bp.route("/hello-world/JSON", methods=["GET"])
+def hello_world_json():
+    return {
+        "name" : "Rhyannon",
+        "message" : "Hi y'all",
+        "hobbies" : ["Coding", "Movies", "Abolition"],
+        }, 201

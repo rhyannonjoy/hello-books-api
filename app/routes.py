a268ace6e5205eec465f13d1fa8e685cd2c79b18
Rhyannon <rhyannonjoy@gmail.com>
2021-10-26 16:17:49 -0600

    All updates except for testing

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,65 @@
-from flask import Blueprint
+from app import db
+from app.models.book import Book
+from flask import Blueprint, jsonify, make_response, request 
 
-hello_world_bp = Blueprint("hello_world", __name__)
 
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def say_hello_world():
-    my_beautiful_response_body = "Hello, World!"
-    return my_beautiful_response_body
+books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-@hello_world_bp.route("/hello-world/JSON", methods=["GET"])
-def hello_world_json():
-    return {
-        "name" : "Rhyannon",
-        "message" : "Hi y'all",
-        "hobbies" : ["Coding", "Movies", "Abolition"],
-        }, 201
+@books_bp.route("", methods=["POST", "GET"])
+def handle_books():
+    if request.method == "POST":
+        request_body = request.get_json()
+        if "title" not in request_body or "description" not in request_body:
+            return make_response("Invalid Request", 400)
+
+        new_book = Book(
+            title=request_body["title"],
+            description=request_body["description"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
+
+        return f"Book {new_book.title} created", 201
+    
+    elif request.method == "GET":
+        title_query = request.args.get("title")
+        if title_query:
+            books = Book.query.filter_by(title=title_query)
+        else:
+            books = Book.query.all()
+        
+        books_response = []
+        for book in books:
+            books_response.append({
+                "id" : book.id,
+                "title" : book.title,
+                "description" : book.description
+            })
+        
+        return jsonify(books_response), 200
+
+@books_bp.route("/<book_id>", methods=["GET", "PUT", "DELETE"])
+def handle_book(book_id):
+    book = Book.query.get(book_id)
+    if book is None:
+        return jsonify(f"Book {book_id} not found"), 404
+
+    if request.method == "GET":
+        return {
+            "id" : book.id,
+            "title" : book.title,
+            "description" : book.description
+        }
+    elif request.method == "PUT":
+        form_data = request.get_json()
+
+        book.title = form_data["title"]
+        book.description = form_data["description"]
+
+        db.session.commit()
+
+        return make_response(f"Book #{book.id} successfully updated.", 200)
+    elif request.method == "DELETE":
+        db.session.delete(book)
+        db.session.commit()
+        return make_response(f"Book #{book.id} successfully deleted.", 200)
---
+++ tests/test_routes.py
@@ -0,0 +1,22 @@
+def test_get_all_books_with_no_records(client):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+
+
+def test_get_one_book(client):
+    # Act
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "id": "",
+        "title": "",
+        "description": ""
+    }

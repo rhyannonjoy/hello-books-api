974b5f5bb2cc12b06ad30292604b7dcfa2f5366a
Rhyannon <rhyannonjoy@gmail.com>
2021-10-26 23:53:42 -0600

    All updated, including tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,7 +8,7 @@ def test_get_all_books_with_no_records(client):
     assert response_body == []
 
 
-def test_get_one_book(client):
+def test_get_one_book(client, two_saved_books):
     # Act
     response = client.get("/books/1")
     response_body = response.get_json()
@@ -16,7 +16,7 @@ def test_get_one_book(client):
     # Assert
     assert response.status_code == 200
     assert response_body == {
-        "id": "",
-        "title": "",
-        "description": ""
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
     }
